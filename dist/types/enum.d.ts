1d009d983fff7bd81626caed64ae4ca59e79ceb7
Sadiq <riomahod@gmail.com>
2024-07-28 15:24:59 +0300

    Generate and include type declaration files

---
+++ dist/types/enum.d.ts
@@ -0,0 +1,29 @@
+export declare enum AkinatorLanguage {
+    English = "en",
+    Arabic = "ar",
+    Chinese = "cn",
+    Germany = "de",
+    Spanish = "es",
+    French = "fr",
+    Italian = "it",
+    Japanese = "jp",
+    Korean = "kr",
+    Dutch = "nl",
+    Polish = "pl",
+    Portuguese = "pt",
+    Russian = "ru",
+    Turkish = "tr",
+    Indonesian = "id"
+}
+export declare enum AkinatorUrlType {
+    Game = "game",
+    Answer = "answer",
+    Back = "cancel_answer"
+}
+export declare enum Answer {
+    Yes = 0,
+    No = 1,
+    IdontKnow = 2,
+    Probably = 3,
+    ProbablyNot = 4
+}
